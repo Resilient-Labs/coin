26ac22744c0cdca1c7969a42b157a34c6276a02f
Miguel.E <112410929+MiguelE-DEV@users.noreply.github.com>
2023-01-04 15:37:52 -0500

    Revert "Nav footer"

--- controllers/posts.js
+++ controllers/posts.js
@@ -61,7 +61,6 @@ module.exports = {
   createPost: async (req, res) => {
     console.log(req.body);
     try {
-      //Use post schema in models to create and save a document to Mongo DB Atlas
       await Post.create({
         category: req.body.category,
         title: req.body.title,
