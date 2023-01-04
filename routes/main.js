1da63fe32441e0c33c82c2658b2abd62facdff8d
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2023-01-04 11:43:51 -0500

    initial commit for incomeInput

--- routes/main.js
+++ routes/main.js
@@ -14,5 +14,7 @@ router.post("/login", authController.postLogin)
 router.get("/logout", authController.logout)
 router.get("/signup", authController.getSignup)
 router.post("/signup", authController.postSignup)
+router.get("/incomeInput", ensureAuth, postsController.getIncome)
+
 
 module.exports = router;
