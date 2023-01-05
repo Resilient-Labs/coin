8a8517f5793ff40b218bbb2c37148a8d83accf36
Abdullahi27 <111465917+Abdullahi27@users.noreply.github.com>
2023-01-05 11:27:37 -0500

    fixed file casing

---
+++ models/Anything.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const IncomeSchema = new mongoose.Schema({
+
+  income: {
+    type: String,
+    required: true,
+  },
+  userid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+ 
+}, { _id : false });
+
+
+
+
+module.exports = mongoose.model("income", IncomeSchema); 
+
---
+++ models/Income.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const IncomeSchema = new mongoose.Schema({
+
+  income: {
+    type: String,
+    required: true,
+  },
+  userid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+ 
+}, { _id : false });
+
+
+
+module.exports = mongoose.model("income", IncomeSchema); 
+
