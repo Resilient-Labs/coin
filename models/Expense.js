0729a9e67bf75df8140fff378c13dbf5ee42be4f
saturnine.mh <zainnasir899@icloud.com>
2023-01-04 19:38:36 -0500

    commit

--- models/Expense.js
+++ models/Expense.js
@@ -1,13 +1,6 @@
 const mongoose = require("mongoose");
 
 const ExpenseSchema = new mongoose.Schema({
-<<<<<<< HEAD
-// <<<<<<< HEAD
-
-// =======
-// >>>>>>> 53e1494d6f7d6f387303e554b9044fb196e69ce1
-  category: {
-=======
 
 
 
@@ -20,7 +13,6 @@ const ExpenseSchema = new mongoose.Schema({
 //     required: true,
 //   },
   expenseCost: {
->>>>>>> cd6064fc1331e551187174269e6236a8cdd79df4
     type: String,
     required: true,
   },
